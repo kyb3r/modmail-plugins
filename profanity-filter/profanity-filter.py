71ad71bb837f3e27e5dd3ebc2cd238ba7c1d963a
kyb3r <abdurraqeeb53@gmail.com>
2019-04-12 23:59:39 +1000

    add whitelist command

--- profanity-filter/profanity-filter.py
+++ profanity-filter/profanity-filter.py
@@ -1,6 +1,8 @@
 import asyncio
 import discord
+from discord import Member, Role
 from discord.ext import commands
+from typing import Union
 
 from profanity_check import predict
 
@@ -21,13 +23,18 @@ class ProfanityFilter:
         self.bot = bot
         self.coll = bot.plugin_db.get_partition(self)
         self.enabled = True
+        self.whitelist = {
+            'roles': [],
+            'users': []
+        }
         asyncio.create_task(self._set_config())
 
     async def _set_config(self):
         config = await self.coll.find_one({'_id': 'config'})
-        self.enabled = config['enabled']
+        self.enabled = config.get('enabled', True)
+        self.whitelist = config.get('whitelist', self.whitelist)
 
-    @commands.command()
+    @commands.group(invoke_without_subcommand=True)
     @commands.is_owner()
     async def profanity(self, ctx, mode: bool):
         """Disable or enable the profanity filter.
@@ -44,8 +51,50 @@ class ProfanityFilter:
         
         await ctx.send(('Enabled' if mode else 'Disabled') + ' the profanity filter.')
     
+    @profanity.command()
+    @commands.is_owner()
+    async def whitelist(self, ctx, role_or_user: Union[Member, Role]):
+        """Whitelist a user or a role from the profanity filter.
+        
+        Usage: `profanity whitelist @dude`
+        """
+
+        key = 'users' if isinstance(role_or_user, Member) else 'roles'
+
+        if role_or_user.id in self.whitelist[key]:
+            self.whitelist[key].remove(role_or_user.id)
+            removed = True
+        else:
+            self.whitelist[key].append(role_or_user.id)
+            removed = False
+
+        await self.coll.update_one(
+            {'_id': 'config'},
+            {'$set': {'whitelist': self.whitelist}}, 
+            upsert=True
+            )
+        
+        await ctx.send(
+            f'{'Un-w' if removed else 'W'}hitelisted '
+            f'{role_or_user.mention} from the profanity filter.'
+            )
+
+    
     async def on_message(self, message):
+
         if not self.enabled:
+            return
+        
+        channel = message.channel
+        author = message.author 
+
+        if channel.permissions_for(author).administrator:
+            return
+
+        if any(r.id in self.whitelist['roles'] for r in author.roles):
+            return
+        
+        if author.id in self.whitelist['users']:
             return
 
         profane = bool(predict([message.content])[0])
@@ -54,8 +103,8 @@ class ProfanityFilter:
 
         await message.delete()
 
-        temp = await message.channel.send(
-            f'{message.author.mention} your message has '
+        temp = await channel.send(
+            f'{author.mention} your message has '
             'been deleted for containing profanity.'
             )
         
